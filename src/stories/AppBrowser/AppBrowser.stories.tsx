60b0ea8c506eedd493059b4ad828dcd828d22a83
Casana Bey <info@expresslayouts.com>
2024-11-03 22:14:03 -0500

    Add dark mode plugin

--- src/stories/AppBrowser/AppBrowser.stories.tsx
+++ src/stories/AppBrowser/AppBrowser.stories.tsx
@@ -1,29 +1,30 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { AppBrowser as Component } from "../../Components";
 import { ThemeProvider } from "styled-components";
-import { theme } from "../../Components/Windows/themes/theme";
+import { theme, darkTheme } from "../../Components/Windows/themes/theme";
+import { useDarkMode } from "storybook-dark-mode";
 
 const meta: Meta<typeof Component> = {
-	title: "Windows 11/AppBrowser",
-	component: Component,
-	parameters: {
-		layout: "centered"
-	},
-	tags: ["autodocs"],
-	argTypes: {},
+  title: "Windows 11/AppBrowser",
+  component: Component,
+  parameters: {
+    layout: "centered"
+  },
+  tags: ["autodocs"],
+  argTypes: {},
 
-	decorators: [
-		Story => (
-			<ThemeProvider theme={theme}>
-				<Story />
-			</ThemeProvider>
-		)
-	]
+  decorators: [
+    Story => (
+      <ThemeProvider theme={useDarkMode() ? darkTheme : theme}>
+        <Story />
+      </ThemeProvider>
+    )
+  ]
 };
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-	args: {}
+  args: {}
 };
