7fe2605a93c2d928a016d65c7dafa9e6b215fa94
Casana Bey <info@expresslayouts.com>
2024-11-07 16:49:00 -0500

    wipper

--- src/Components/Windows/Header/Header.styles.ts
+++ src/Components/Windows/Header/Header.styles.ts
@@ -8,4 +8,93 @@ export const HeaderWrapper = Styled.header`
     position: fixed;
     box-shadow: ${({ theme }) => theme.boxShadows.small};
     background-color: ${props => props.theme.colors.primaryBgColor};
+
+    display: grid;
+    grid-template-columns: 220px 1fr 220px;
+    padding: 0 1rem;
+    border-bottom: .0625rem solid ${({ theme }) => theme.colors.borderColor};
 `;
+
+export const LogoWrapper = Styled.div`
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: ${({ theme }) => theme.colors.text};
+    border-right: .0625rem solid ${({ theme }) => theme.colors.borderColor};
+`;
+
+export const ButtonWrapper = Styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    > button:first-child {
+        border-left: .0625rem solid ${({ theme }) => theme.colors.borderColor};
+    }
+`;
+
+export const HeaderButton = Styled.button`
+    background-color: transparent;
+    border: none;
+    border-right: .0625rem solid ${({ theme }) => theme.colors.borderColor};
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 1rem;
+    padding: .5rem;
+    transition: color .2s;
+    outline: none;
+    width: 3.125rem;
+    height: 3.125rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    >svg{
+        padding:0;
+        margin:0;
+    }
+
+    &:hover {
+        color: ${({ theme }) => theme.colors.textHover};
+    }
+
+    &:active {
+        color: ${({ theme }) => theme.colors.textActive};
+    }
+
+    &:focus {
+        color: green
+    }
+`;
+
+export const UserWrapper = Styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: .5rem;
+`;
+
+export const UserAvatar = Styled.div`
+    width: 2rem;
+    height: 2rem;
+    border-radius: .25rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.gray6};
+    font-size: 1rem;
+    font-weight: 700;
+`;
+
+export const UserInfo = Styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: .25rem;
+    color: ${({ theme }) => theme.colors.text};
+    font-size: .75rem;
+    line-height: 1rem;
+`;
+
+// 50 / 16 = 3.125rem
--- src/Components/Windows/Header/Header.tsx
+++ src/Components/Windows/Header/Header.tsx
@@ -1,10 +1,26 @@
 import React, { FC } from "react";
 import * as Styled from "./Header.styles";
 
+import { ChevronDown } from "react-feather";
+
 export const Header: FC = () => {
-	return (
-		<Styled.HeaderWrapper>
-			<h1>Header</h1>
-		</Styled.HeaderWrapper>
-	);
+  return (
+    <Styled.HeaderWrapper>
+      <Styled.LogoWrapper>MM</Styled.LogoWrapper>
+      <Styled.ButtonWrapper>
+        <Styled.HeaderButton aria-label="show app browser">
+          <ChevronDown />
+        </Styled.HeaderButton>
+      </Styled.ButtonWrapper>
+
+      <Styled.UserWrapper>
+        <Styled.UserAvatar>CB</Styled.UserAvatar>
+        <Styled.UserInfo>
+          Casana Bey
+          <br />
+          Casana.Bey@email.com
+        </Styled.UserInfo>
+      </Styled.UserWrapper>
+    </Styled.HeaderWrapper>
+  );
 };
